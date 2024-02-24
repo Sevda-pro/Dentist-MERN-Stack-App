c7300e9386dfa5e64613809b00812ff2d5e4aefa
Sevda-pro <mesahilsevda@gmail.com>
2024-02-24 13:59:56 +0530

    signup page improved

--- frontend/src/components/signup/signup.js
+++ frontend/src/components/signup/signup.js
@@ -4,24 +4,35 @@ import { useNavigate } from 'react-router-dom'
 import { FloatingLabel, Button } from 'react-bootstrap';
 import Form from 'react-bootstrap/Form';
 import axios from 'axios';
+import Alert from 'react-bootstrap/Alert';
 export default function Signup() {
     const [name, setName] = useState('')
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
+    const [signupalert, setAlert] = useState(false)
     const navigate = useNavigate();
     async function signup(e) {
         e.preventDefault();
-        let obj = {
-            name: name,
-            email: email,
-            password: password
+        if (name == '' || email == '' || password == '') {
+            setAlert(true);
+            setTimeout(() => {
+                setAlert(false)
+            }, 2000);
         }
-        let res = await axios.post(`${process.env.REACT_APP_API_KEY}`, obj)
-        alert(res.data.message);
-        setName('');
-        setEmail('');
-        setPassword('');
-        signin();
+        else {
+            let obj = {
+                name: name,
+                email: email,
+                password: password
+            }
+            let res = await axios.post(`${process.env.REACT_APP_API_KEY}`, obj)
+            alert(res.data.message);
+            setName('');
+            setEmail('');
+            setPassword('');
+            signin();
+        }
+
     }
     function signin() {
         navigate('/login')
@@ -29,15 +40,22 @@ export default function Signup() {
     return (
 
         <div className="signup">
+            {signupalert && (
+                <div className='alertclass'>
+                        Please fill all the details!
+                </div>
+
+
+            )}
             <div className='frame'>
                 <div className='shape'>
                     <div className='formdiv'>
                         <label>Name:</label>
-                        <input type='text' value={name} onChange={(e)=>setName(e.currentTarget.value)}></input>
+                        <input type='text' value={name} onChange={(e) => setName(e.currentTarget.value)} required></input>
                         <label>Email:</label>
-                        <input type='email' value={email} onChange={(e)=>setEmail(e.currentTarget.value)}></input>
+                        <input type='email' value={email} onChange={(e) => setEmail(e.currentTarget.value)} required></input>
                         <label>Password:</label>
-                        <input type='password' value={password} onChange={(e)=>setPassword(e.currentTarget.value)}></input>
+                        <input type='password' value={password} onChange={(e) => setPassword(e.currentTarget.value)} required></input>
                         <button className='signupbtn' onClick={signup}>Signup</button>
                         <button className='loginbtn' onClick={signin}>Signin</button>
 
